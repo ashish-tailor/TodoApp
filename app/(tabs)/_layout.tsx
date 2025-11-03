acc4884e988f62b3823476054fa821637ca1405e
Ashish Tailor <ashishtailorbhl@gmail.com>
2025-11-03 21:18:47 +0530

    Created context for theme changes

---
+++ app/(tabs)/_layout.tsx
@@ -0,0 +1,50 @@
+import useTheme from "@/hooks/useTheme";
+import { Ionicons } from "@expo/vector-icons";
+import { Tabs } from "expo-router";
+import React from "react";
+import { StyleSheet } from "react-native";
+const TabsLayout = () => {
+  const { colors } = useTheme();
+  return (
+    <Tabs
+      screenOptions={{
+        tabBarActiveTintColor: colors.primary,
+        tabBarInactiveTintColor: colors.textMuted,
+        tabBarStyle: {
+          backgroundColor: colors.surface,
+          borderWidth: 1,
+          borderTopColor: colors.border,
+          paddingTop: 10,
+        },
+        tabBarLabelStyle: {
+          fontSize: 12,
+          fontWeight: "600",
+        },
+        headerShown: false,
+      }}
+    >
+      <Tabs.Screen
+        name="index"
+        options={{
+          title: "Todos",
+          tabBarIcon: ({ color, size }) => (
+            <Ionicons name="flash-outline" size={size} color={color} />
+          ),
+        }}
+      />
+      <Tabs.Screen
+        name="settings"
+        options={{
+          title: "Settings",
+          tabBarIcon: ({ color, size }) => (
+            <Ionicons name="settings" size={size} color={color} />
+          ),
+        }}
+      />
+    </Tabs>
+  );
+};
+
+export default TabsLayout;
+
+const styles = StyleSheet.create({});
---
+++ app/(tabs)/settings.tsx
@@ -0,0 +1,20 @@
+import React from "react";
+import { StyleSheet, Text, View } from "react-native";
+
+const SettingScreen = () => {
+  return (
+    <View style={styles.container}>
+      <Text>SettingScreen</Text>
+    </View>
+  );
+};
+
+export default SettingScreen;
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+  },
+});
